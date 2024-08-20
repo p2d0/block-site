87f6fb21807ae7854682c42174843f11fb4825d5
patriot720 <cerkin-3@yandex.ru>
2024-08-20 13:34:21 +0300

    add order to the rules
    So you can do things like
    reddit.com
    !https://www.reddit.com/r/*
    https://www.reddit.com/r/all/
    
    Every subreddit unblocked except /r/all

--- src/helpers/make-rules.ts
+++ src/helpers/make-rules.ts
@@ -8,16 +8,9 @@ export interface Rule {
 }
 
 export default (blocked: string[]): Rule[] => {
-  const allowList = blocked
-    .filter((item) => item.startsWith("!"))
-    .map((item) => removeProtocol(item.substring(1)));
-
-  const blockList = blocked
-    .filter((item) => !item.startsWith("!"))
-    .map(removeProtocol);
-
-  return [
-    ...allowList.map((path) => ({ type: "allow", path } as Rule)),
-    ...blockList.map((path) => ({ type: "block", path } as Rule)),
-  ];
+  return blocked.map((item) => {
+    const isAllow = item.startsWith("!");
+    const path = removeProtocol(isAllow ? item.substring(1) : item);
+    return { type: isAllow ? "allow" : "block", path } as Rule;
+  }).reverse();
 };
